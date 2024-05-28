da05e5d2a3b8e32b91f6587eac593f2c97a2aab8
MaMiki-dot <ya.schumik@yandex.ru>
2024-05-28 17:50:44 +0300

    renew

--- app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = "https://kebab.nomoredomainswork.ru/api";
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
-};
